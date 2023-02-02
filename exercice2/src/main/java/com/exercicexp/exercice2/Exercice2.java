c8bdd38e40ab969c101007fb7fca0e2999a5755a
macbook <wkoua@oxylead.net>
2023-02-02 06:07:50 +0000

    commit'

---
+++ exercice2/src/main/java/com/exercicexp/exercice2/Exercice2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.exercicexp.exercice2;
+
+/**
+ *
+ * @author wilfried koua
+ */
+public class Exercice2 {
+
+    public static void main(String[] args) {
+        
+         // Boucle pour les lignes
+         
+        for (int iteration1 = 1; iteration1 <= 7; iteration1++) {
+            for (int interation2 = 1; iteration2 <= interation2; iteration2++) {
+                System.out.print(iteration1);
+            }
+            System.out.println();
+        }
+    
+    }
+}
